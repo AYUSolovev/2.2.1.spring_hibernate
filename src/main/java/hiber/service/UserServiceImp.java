7e0b1e51826b97a53f8c10c475a5b9ff03ec59cf
Solovev Anatoliy <tolya-ptisa@mail.ru>
2021-03-18 00:46:17 +0300

    18032021 0046

---
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -0,0 +1,44 @@
+package hiber.service;
+
+import hiber.dao.UserDao;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Transactional
+    @Override
+    public void add(User user) {
+        userDao.add(user);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> listUsers() {
+        return userDao.listUsers();
+    }
+
+    @Transactional
+    @Override
+    public User getUserByCar(String model, int series) {
+        Query query = sessionFactory.getCurrentSession()
+                .createQuery("from User where car.model = :paramModel and car.series = :paramSeries");
+        query.setParameter("paramModel", model)
+                .setParameter("paramSeries", series);
+        List<User> user = query.list();
+        return user.get(0);
+    }
+}
